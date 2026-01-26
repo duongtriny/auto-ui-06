7d4c9d7ba156f0602f8aac7a58c48ed2c1ea8632
Duong Trinh <duong.triny@gmail.com>
2026-01-26 21:33:26 +0700

    [ID-37] Manage multiple environment

--- model/utils/constants-utils.ts
+++ model/utils/constants-utils.ts
@@ -1,6 +1,8 @@
-export const UI_HOST = 'http://localhost';
-export const UI_PORT = '3000';
+import { getEnv } from "./config-utils";
+
+export const UI_HOST = getEnv('UI_HOST');
+export const UI_PORT = getEnv('UI_PORT');
 export const UI_BASE_URL = `${UI_HOST}${UI_PORT ? ':' + UI_PORT : ''}`;
 export const UI_ADMIN_LOGIN_URL = `${UI_BASE_URL}/admin/login`;
-export const ADMIN_USERNAME = 'test@with.me';
-export const ADMIN_PASSWORD = '1234567890';
+export const ADMIN_USERNAME = getEnv('ADMIN_USERNAME');
+export const ADMIN_PASSWORD = getEnv('ADMIN_PASSWORD');
